ff60bebd07ddb0cfc4e1f2bf21fbb35d08f4b7be
Calloumi-Bambooki <duck-vader@hotmail.co.uk>
2017-05-31 22:34:24 +0100

    .

--- src/screen.erl
+++ src/screen.erl
@@ -10,8 +10,8 @@ init() ->
   python:call(P, builtins, print, [<<"Hello, World!">>]),
   wait(),
 %  io:fwrite(python:call(P, screen, moo, [])),
-%  io:fwrite(python:call(P, '3d_box', moo, [])),
-  io:fwrite(python:call(P, 'demo_opts', get_device, [])),
+  io:fwrite(python:call(P, '3d_box', moo, [])),
+%  io:fwrite(python:call(P, 'demo_opts', get_device, [])),
   wait(),
   {ok, Dir}=file:get_cwd(),
   io:fwrite(Dir).
