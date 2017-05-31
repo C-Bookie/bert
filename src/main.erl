5d1c88a36c7f83600a65ad94b4dd065b09add26a
Calloumi-Bambooki <duck-vader@hotmail.co.uk>
2017-05-31 20:42:06 +0100

    .

--- src/main.erl
+++ src/main.erl
@@ -1,18 +1,19 @@
 
 -module(main).
 -compile(export_all).
+%-include("util.erl").
 
 moo() ->
-  screen:init().
+  screen:init(),
+  ok.
 
-main() ->
+run() ->
   N=util:makeNurons("./maps/bert.map"),
 %  N=util:makeNurons("./maps/small.map"),
   io:fwrite("started, press enter to stop shocking and again to end~n"),
 	S=spawn(input, shock, [N]),
 	spawn_link(paint, pulse, [N, 0]),
   io:get_line(""),
-  io:fwrite("shock stopped~n"),
   exit(S, "shock stopped"),
   io:get_line(""),
   exit("ended").
--- src/screen.erl
+++ src/screen.erl
@@ -1,26 +1,18 @@
-%%%-------------------------------------------------------------------
-%%% @author duck-
-%%% @copyright (C) 2017, <COMPANY>
-%%% @doc
-%%%
-%%% @end
-%%% Created : 27. May 2017 19:26
-%%%-------------------------------------------------------------------
--module(screen).
--author("duck-").
 
-%% API
+-module(screen).
 -compile(export_all).
 
 init() ->
-  {ok, P} = python:start_link([{python_path, "python/"}]),
+  {ok, P} = python:start_link([{python_path, "/home/pi/python/luma.examples/examples"}]),
 %  {ok, P} = python:start([{python, "python3"}]),
 %  {ok, P} = python:start_link(),
   wait(),
   python:call(P, builtins, print, [<<"Hello, World!">>]),
   wait(),
-  io:fwrite(python:call(P, screen, moo, [])),
-  wait(),
+%  io:fwrite(python:call(P, screen, moo, [])),
+  io:fwrite(python:call(P, '3d_box', main, [])),
+
+wait(),
   {ok, Dir}=file:get_cwd(),
   io:fwrite(Dir).
 
