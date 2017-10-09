6b74968c3c4c761c856242cab8eb382d17828f5e
Calloumi-Bambooki <duck-vader@hotmail.co.uk>
2017-10-09 14:43:26 +0100

    push to pi for testing

--- java/tic tac toe/src/com/company/Main.java
+++ java/tic tac toe/src/com/company/Main.java
@@ -4,16 +4,20 @@ import java.io.IOException;
 
 public class Main {
 	
-	public static void main(String[] args)  throws IOException {
+	public static void main(String[] args) {
 //		Main main = new Main();
 //		System.out.println(main.self.createPid().toString());
 // 		main.run();
 		
 		
 		Pi pi = new Pi();
-		
-		pi.foo();
-		
+
+		try {
+			pi.foo();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 	
 }
