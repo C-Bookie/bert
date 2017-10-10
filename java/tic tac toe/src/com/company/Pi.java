265395c2b3c18ec245fb47ddc28633fe9bca0744
Calloumi-Bambooki <duck-vader@hotmail.co.uk>
2017-10-10 14:13:57 +0100

    push to pi for testing

--- java/tic tac toe/src/com/company/Pi.java
+++ java/tic tac toe/src/com/company/Pi.java
@@ -3,8 +3,8 @@ package com.company;
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.RaspiPin;
-import com.pi4j.io.spi.SpiChannel;
-import com.pi4j.io.spi.SpiFactory;
+import com.pi4j.io.i2c.I2CBus;
+import com.pi4j.io.i2c.I2CFactory;
 
 import java.io.IOException;
 
@@ -12,7 +12,9 @@ import java.io.IOException;
  * Created by duck- on 08/10/2017.
  */
 public class Pi {
-	
+
+    final static int address = 0x32;
+
 	public Pi() {
 
 	}
@@ -20,12 +22,16 @@ public class Pi {
 	public static void foo()  throws IOException {
 		System.out.println("boo ya");
 
-		GpioController gpio = GpioFactory.getInstance();
+        Display disp = null;
+        try {
+            disp = new Display(128, 64, GpioFactory.getInstance(), I2CFactory.getInstance(I2CBus.BUS_1), address, RaspiPin.GPIO_07);
+        } catch (I2CFactory.UnsupportedBusNumberException e) {
+            e.printStackTrace();
+        } catch (ReflectiveOperationException e) {
+            e.printStackTrace();
+        }
 
-//		Display disp = new Display(128, 64, GpioFactory.getInstance(), SpiFactory.getInstance(SpiChannel.CS1, 8000000), RaspiPin.GPIO_03, RaspiPin.GPIO_04);
-		Display disp = new Display(128, 64, gpio, SpiFactory.getInstance(SpiChannel.CS1, 8000000), RaspiPin.GPIO_03, RaspiPin.GPIO_04);
-
-		disp.begin();
+        disp.begin();
 
 		long last, nano = 0;
 
